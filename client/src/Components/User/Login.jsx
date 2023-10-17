213f29c522b2c2e35dab7d0cae48ac5cf469df8e
Abhishek-kumar41 <127575574+AbhishekAK411@users.noreply.github.com>
2023-10-18 00:15:20 +0530

    changes

--- client/src/Components/User/Login.jsx
+++ client/src/Components/User/Login.jsx
@@ -5,7 +5,10 @@ import { toast } from "react-hot-toast";
 import api from "../../Utils/Axiosconfig";
 
 const Login = ( { registerMode, onRegisterToggle } ) => {
-  const [userData, setUserData] = useState({field: "", password: ""});
+  const [userData, setUserData] = useState({
+    field: "",
+    password: ""
+  });
 
     const handleChange = (e) => {
       setUserData({...userData, [e.target.name]: e.target.value})
