f1f68211541e9652e0d2dd26754c4972128a872e
Abhishek-kumar41 <127575574+AbhishekAK411@users.noreply.github.com>
2023-10-17 18:27:52 +0530

    updates on login component

--- client/src/Components/User/Login.jsx
+++ client/src/Components/User/Login.jsx
@@ -55,7 +55,7 @@ const Login = ( { registerMode, onRegisterToggle } ) => {
                   <Input
                     onChange={handleChange}
                     name="username"
-                    label="Username"
+                    label="Username or Email"
                     className="focus:border-gray-900 text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
                     type="text"
                   />
