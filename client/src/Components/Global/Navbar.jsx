7e91bb7dd990eaf36528ac7fe2e166d75cc80e70
Abhishek-kumar41 <127575574+AbhishekAK411@users.noreply.github.com>
2023-10-18 17:52:53 +0530

    updates on navbar animations

--- client/src/Components/Global/Navbar.jsx
+++ client/src/Components/Global/Navbar.jsx
@@ -2,14 +2,29 @@ import { Button, Avatar } from "@material-tailwind/react";
 import { useContext } from "react";
 import { authContext } from "../../Context/Authcontext";
 import userDummyImage from "../../Resources/Images/userDummyImage.png";
+import { motion } from "framer-motion";;
 
 const Navbar = ({ onLoginToggle }) => {
 
     const {state} = useContext(authContext);
     const username = state?.user?.username;
+
+    const navbarVariants = {
+        initial : {
+            y: -180
+        },
+        animate: {
+            y: 0
+        }
+    }
+    const transition = {
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+    }
     return (
         <>
-            <nav className="select-none fixed top-0 w-full h-[80px] flex items-center justify-center">
+            <motion.nav variants={navbarVariants} initial="initial" animate="animate" transition={transition} className="select-none fixed top-0 w-full h-[80px] flex items-center justify-center">
                 <section className="w-[40%] h-[60%] border rounded-full backdrop-blur-lg shadow-xl flex items-center justify-evenly">
                     <Button size="sm" variant="text" className="h-[80%] rounded-full text-gray-500">Home</Button>
                     <Button size="sm" variant="text" className="h-[80%] rounded-full text-gray-500">Feed</Button>
@@ -17,7 +32,7 @@ const Navbar = ({ onLoginToggle }) => {
                     <Button size="sm" variant="text" className="h-[80%] rounded-full text-gray-500">Features</Button>
                     {username ? (<><Button size="xs" variant="text" className="rounded-full h-[80%] flex items-center justify-center text-gray-500"><Avatar src={userDummyImage} size="xs" withBorder={true} className="p-0.5" /></Button></>) : (<><Button onClick={onLoginToggle} size="sm" variant="text" className="h-[80%] rounded-full text-gray-500">Sign In</Button></>)}
                 </section>
-            </nav>
+            </motion.nav>
         </>
     )
 }
