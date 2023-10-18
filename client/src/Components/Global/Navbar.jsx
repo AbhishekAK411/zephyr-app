61d2254bab8faab39432924ebe7af6a7dbc84f5a
Abhishek-kumar41 <127575574+AbhishekAK411@users.noreply.github.com>
2023-10-18 17:37:36 +0530

    added dummyImage and updated navbar and navigation component with global state management

--- client/src/Components/Global/Navbar.jsx
+++ client/src/Components/Global/Navbar.jsx
@@ -1,19 +1,21 @@
-import { Button } from "@material-tailwind/react";
+import { Button, Avatar } from "@material-tailwind/react";
 import { useContext } from "react";
 import { authContext } from "../../Context/Authcontext";
+import userDummyImage from "../../Resources/Images/userDummyImage.png";
 
 const Navbar = ({ onLoginToggle }) => {
 
     const {state} = useContext(authContext);
+    const username = state?.user?.username;
     return (
         <>
-            <nav className="fixed top-0 w-full h-[80px] flex items-center justify-center">
+            <nav className="select-none fixed top-0 w-full h-[80px] flex items-center justify-center">
                 <section className="w-[40%] h-[60%] border rounded-full backdrop-blur-lg shadow-xl flex items-center justify-evenly">
                     <Button size="sm" variant="text" className="h-[80%] rounded-full text-gray-500">Home</Button>
                     <Button size="sm" variant="text" className="h-[80%] rounded-full text-gray-500">Feed</Button>
                     <Button size="sm" variant="text" className="h-[80%] rounded-full text-gray-500">Explore</Button>
                     <Button size="sm" variant="text" className="h-[80%] rounded-full text-gray-500">Features</Button>
-                    <Button onClick={onLoginToggle} size="sm" variant="text" className="h-[80%] rounded-full text-gray-500">Sign In</Button>
+                    {username ? (<><Button size="xs" variant="text" className="rounded-full h-[80%] flex items-center justify-center text-gray-500"><Avatar src={userDummyImage} size="xs" withBorder={true} className="p-0.5" /></Button></>) : (<><Button onClick={onLoginToggle} size="sm" variant="text" className="h-[80%] rounded-full text-gray-500">Sign In</Button></>)}
                 </section>
             </nav>
         </>
--- client/src/Components/Global/Navigation.jsx
+++ client/src/Components/Global/Navigation.jsx
@@ -1,8 +1,18 @@
 import { IconButton, Tooltip } from "@material-tailwind/react";
 import { motion } from "framer-motion";
+import { useContext } from "react";
+import { authContext } from "../../Context/Authcontext";
+import { toast } from "react-hot-toast";
 
 const Navigation = () => {
 
+    const {state, logout} = useContext(authContext);
+    const username = state?.user?.username;
+
+    const handleLogout = () => {
+        logout();
+        toast.success("logged out successfully.");
+    }
     const animate = {
         mount: {
             scale: 1,
@@ -30,11 +40,11 @@ const Navigation = () => {
     };
     return (
         <>
-            <motion.section initial={{x: 180}} animate={{x: 0}} transition={{delay: 1}} className="w-[4%] h-[350px] border fixed right-5 top-52 backdrop-blur-sm rounded-full shadow-2xl flex flex-col items-center justify-evenly">
+            <motion.section initial={{x: 180}} animate={{x: 0}} transition={{delay: 1}} className="select-none w-[4%] h-[350px] border fixed right-5 top-52 backdrop-blur-sm rounded-full shadow-2xl flex flex-col items-center justify-evenly">
                 <motion.div variants={buttonVariants} initial="initial" animate="animate" transition={transition}>
-                    <Tooltip content="Home" className="bg-gray-900 rounded-full border text-white ml-[-10px]" placement="left" animate={animate}>
+                    <Tooltip content="Conversations" className="bg-gray-900 rounded-full border text-white ml-[-10px]" placement="left" animate={animate}>
                         <IconButton className="rounded-full text-[#800000] text-base" variant="text">
-                            <i className="fa fa-home" aria-hidden="true"></i>
+                            <i className="fa fa-comments" aria-hidden="true"></i>
                         </IconButton>
                     </Tooltip>
                 </motion.div>
@@ -59,13 +69,22 @@ const Navigation = () => {
                         </IconButton>
                     </Tooltip>
                 </motion.div>
-                <motion.div variants={buttonVariants} initial="initial" animate="animate" transition={transition}>
+                {username ? (<><motion.div onClick={handleLogout} variants={buttonVariants} initial="initial" animate="animate" transition={transition}>
                     <Tooltip content="Logout" className="bg-gray-900 rounded-full border text-white ml-[-10px]" placement="left" animate={animate}>
                         <IconButton className="rounded-full text-[#800000] text-base" variant="text">
                             <i className="fa fa-sign-out" aria-hidden="true"></i>
                         </IconButton>
                     </Tooltip>
+                </motion.div></>) :
+                (<>
+                    <motion.div variants={buttonVariants} initial="initial" animate="animate" transition={transition}>
+                    <Tooltip content="Schedule Call" className="bg-gray-900 rounded-full border text-white ml-[-10px]" placement="left" animate={animate}>
+                        <IconButton className="rounded-full text-[#800000] text-base" variant="text">
+                            <i className="fa fa-phone" aria-hidden="true"></i>
+                        </IconButton>
+                    </Tooltip>
                 </motion.div>
+                </>)}
             </motion.section>
         </>
     )
