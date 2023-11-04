f71e5f931c6fb30c5566232c1ed99416fd66987c
Abhishek-kumar41 <127575574+AbhishekAK411@users.noreply.github.com>
2023-11-04 16:03:53 +0530

    updates on card component

--- client/src/Components/Card/Blogcard.jsx
+++ client/src/Components/Card/Blogcard.jsx
@@ -14,10 +14,7 @@ const Blogcard = () => {
         <section className="w-[90%] h-[500px] flex items-center justify-center rounded-lg">
           <Card className="mt-6 w-[90%] shadow-stripe">
             <CardHeader color="blue-gray" className="relative h-56">
-              <img
-                src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-                alt="card"
-              />
+              <img src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="card" />
             </CardHeader>
             <CardBody>
               <Typography variant="h5" color="blue-gray" className="mb-2">
