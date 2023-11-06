82e07b380507492afde0d521920058a2f01def58
Abhishek-kumar41 <127575574+AbhishekAK411@users.noreply.github.com>
2023-11-07 00:24:40 +0530

    updates on routers

--- server/src/routes/blogRoutes.ts
+++ server/src/routes/blogRoutes.ts
@@ -1,10 +1,11 @@
-import { addBlog, getAllBlogs } from "../controllers/blog.cont";
+import { addBlog, getAllBlogs, getSingleBlog } from "../controllers/blog.cont";
 import express,{Router} from "express";
-import { checkAddBlog, checkGetAllBlogs } from "../middlewares/blogAuth";
+import { checkAddBlog, checkGetAllBlogs, checkGetSingleBlog } from "../middlewares/blogAuth";
 
 const blogRouter: Router = express.Router();
 
 blogRouter.post("/create", checkAddBlog, addBlog);
 blogRouter.post("/feed", checkGetAllBlogs, getAllBlogs);
+blogRouter.post("/getSingle", checkGetSingleBlog, getSingleBlog);
 
 export default blogRouter;
