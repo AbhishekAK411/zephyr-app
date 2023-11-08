15fd7c7601df9928403e1a32db24b456c705903a
Abhishek-kumar41 <127575574+AbhishekAK411@users.noreply.github.com>
2023-11-08 22:58:59 +0530

    updates on backend and updates on client side components parsing package installed for parsing html string to html code

--- client/src/Components/Blogs/Singleblog.jsx
+++ client/src/Components/Blogs/Singleblog.jsx
@@ -3,43 +3,61 @@ import { toast } from "react-hot-toast";
 import { useParams } from "react-router-dom";
 import blogApi from "../../Utils/Blogconfig";
 import { authContext } from "../../Context/Authcontext";
-import Blogpost from "../Card/Blogpost";
+import {Typography} from "@material-tailwind/react";
+import parse from "html-react-parser";
 
 const Singleblog = () => {
-    const [singleBlogData, setSingleBlogData] = useState();
-    const {id} = useParams();
-    const {state} = useContext(authContext);
-    
-    //* This is to fetch single blog data from server.
-    useEffect(() => {
-        if(state?.user?._id){
-            const getSingleBlogData = async() => {
-                try {
-                    const response = await blogApi.post("/getSingle", {
-                        userId: state?.user?._id,
-                        blogId: id
-                    });
-                    const axiosResponse = response?.data;
-                    if(axiosResponse?.success){
-                        setSingleBlogData(axiosResponse.singleBlog);
-                    }
-                } catch (error) {
-                    toast.error(error?.response?.data?.message);
-                }
-            }
-            getSingleBlogData();
-        }
-    }, [state?.user?._id, id]);
+  const [singleBlogData, setSingleBlogData] = useState();
+  const [authorData, setAuthorData] = useState();
+  const { id } = useParams();
+  const { state } = useContext(authContext);
 
-    return (
-        <>
-            <div className="w-full h-screen border-black border flex items-center justify-center">
-                <div className="w-[95%] h-screen flex items-center justify-center">
-                    <Blogpost post={singleBlogData} />
-                </div>
-            </div>
-        </>
-    )
-}
+  //* This is to fetch single blog data from server.
+  useEffect(() => {
+    if (state?.user?._id && id) {
+      const getSingleBlogData = async () => {
+        try {
+          const response = await blogApi.post("/getSingle", {
+            userId: state?.user?._id,
+            blogId: id,
+          });
+          const axiosResponse = response?.data;
+          if (axiosResponse?.success) {
+            setSingleBlogData(axiosResponse?.singleBlog);
+            setAuthorData(axiosResponse?.author);
+          }
+        } catch (error) {
+          toast.error(error?.response?.data?.message);
+        }
+      };
+      getSingleBlogData();
+    }
+  }, [state?.user?._id, id]);
+
+  let descriptionToDisplay = "";
+  if(singleBlogData){
+    descriptionToDisplay = parse(singleBlogData?.description);
+  }
+  return (
+    <>
+      <div className="w-full h-screen border-black border flex items-center justify-center">
+        <div className="w-[95%] h-screen flex items-center justify-center">
+          <div className="pl-10 pt-10 w-[95%] h-[95%]">
+            <Typography variant="h3">{singleBlogData?.title}</Typography>
+            <Typography variant="h6" className="text-gray-500 mt-5 font-normal">
+              {authorData}
+            </Typography>
+            <Typography variant="h6" className="text-gray-500 font-normal">
+              Timestamp
+            </Typography>
+            <Typography variant="paragraph" className="mt-10">
+              {descriptionToDisplay}
+            </Typography>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Singleblog;
--- client/src/Components/Card/Blogpost.jsx
+++ client/src/Components/Card/Blogpost.jsx
@@ -1,21 +1,11 @@
 import {Typography} from "@material-tailwind/react";
+import parse from "html-react-parser";
 
-const Blogpost = ({post}) => {
-    console.log(post);
+const Blogpost = ({post,author}) => {
+
   return (
     <>
-      <div className="pl-10 pt-10 w-[95%] h-[95%]">
-        <Typography variant="h3">Title</Typography>
-        <Typography variant="h6" className="text-gray-500 mt-5 font-normal">
-          Author
-        </Typography>
-        <Typography variant="h6" className="text-gray-500 font-normal">
-          Timestamp
-        </Typography>
-        <Typography variant="paragraph" className="mt-10">
-          Paragraph
-        </Typography>
-      </div>
+      
     </>
   );
 };
